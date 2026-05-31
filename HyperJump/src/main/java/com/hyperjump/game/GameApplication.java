14fd2b1f788c92d990c5e98e0ad22e6f3065ebef
SirRmelTheThird <armelbissaka32@gmail.com>
2026-05-31 11:33:30 +0100

    Update GameApplication.java

--- HyperJump/src/main/java/com/hyperjump/game/GameApplication.java
+++ HyperJump/src/main/java/com/hyperjump/game/GameApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GameApplication {
-
     static void main(String[] args) {
         SpringApplication.run(GameApplication.class, args);
     }
-
 }
